251d2d9124bf81bf037ee9f02956108342c4ec80
Dmitrij Podabed <dmitrij@podabed.org>
2019-06-03 16:31:34 +0300

    refactor 404

--- src/pages/404.ru.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-import Layout from '../components/Layout';
-
-const NotFoundPage: React.FC = (props) => <Layout location={props.location}>404</Layout>;
-
-export default NotFoundPage;
